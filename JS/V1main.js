8af517ad32c21aa4822187e03563b07fc6ce69c1
Freejinn <noonfirerebel@gmail.com>
2023-09-11 12:45:08 -0600

    Created random quote generator

--- JS/V1main.js
+++ JS/V1main.js
@@ -1,56 +1,95 @@
-const addSignupModal_el = document.getElementById("signup-modal");
-const signupBtn_el = document.getElementById("signup-btn");
+const addSignupModal_el = document.getElementById('signup-modal');
+const signupBtn_el = document.getElementById('signup-btn');
 const backdrop_el = document.getElementById('backdrop');
 const closeBtn_el = document.getElementById('close-btn');
 const userInputs_el = document.querySelectorAll('input');
 const clearButton_el = document.getElementById('clear-btn');
 const textAreaInput_el = document.getElementById('text-area');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
 
-document.querySelectorAll('nav a').forEach(link => {
-    if(link.href === window.location.href) {
-        link.setAttribute('aria-current', 'page')
-    };
-})
+
+document.querySelectorAll('nav a').forEach((link) => {
+  if (link.href === window.location.href) {
+    link.setAttribute('aria-current', 'page');
+  }
+});
+
+const generateQuote = function() {
+    const quotes = [
+      {
+        quote:
+          'There are no limits. There are only plateaus, and you must not stay there. You must go beyond them.',
+        author: '~ Bruce Lee ~',
+      },
+      {
+        quote: "I train as many arts as possible. I'm constantly learning all the time. Once you stop being a student, you stop growing.",
+        author: '~ Dan Insosanto ~' 
+      },
+      {
+          quote: "Mastering others is strength; mastering yourself is true power.",
+          author: "~ Lao Tzu ~"
+      },
+      {
+          quote: "Impossible is just a word thrown around my small men who find it easier to live in the world they've been given than to explore the power they have to change it. Impossible is not a fact, it's an opinion.",
+          author: "~ Muhammad Ali ~"
+      }
+    ];
+    // console.log("Hi");
+
+    let arrayIndex = Math.floor(Math.random() * quotes.length);
+    quote.innerHTML = quotes[arrayIndex].quote;
+    author.innerHTML = quotes[arrayIndex].author;
+  }
+
+  window.onload = function() {
+    generateQuote();
+  }
+
 
 const toggleBackdrop = () => {
-    backdrop_el.classList.toggle('visible');
-  };
+  backdrop_el.classList.toggle('visible');
+};
 
 const openSignupModal = () => {
-    addSignupModal_el.classList.add('visible');
-    toggleBackdrop();
+  addSignupModal_el.classList.add('visible');
+  toggleBackdrop();
 };
 
 const closeSignupModal = () => {
-    addSignupModal_el.classList.remove('visible');
-    backdrop_el.classList.remove('visible');
-    clearUsrInput();
-    textAreaInput_el.value = '';
-}
+  addSignupModal_el.classList.remove('visible');
+  backdrop_el.classList.remove('visible');
+  clearUsrInput();
+  textAreaInput_el.value = '';
+};
 
 const clearUsrInput = () => {
-    for (userInpt of userInputs_el) {
-        userInpt.value = '';
-    }
-    textAreaInput_el.value = '';
-}
+  for (userInpt of userInputs_el) {
+    userInpt.value = '';
+  }
+  textAreaInput_el.value = '';
+};
 
-signupBtn_el.addEventListener("click", openSignupModal);
+signupBtn_el.addEventListener('click', openSignupModal);
 backdrop_el.addEventListener('click', closeSignupModal);
 closeBtn_el.addEventListener('click', closeSignupModal);
 clearButton_el.addEventListener('click', clearUsrInput);
 
+
+  
 let counter = 0;
 displayImages();
-function displayImages(){
-    const images = document.getElementsByClassName("image");
-    for (let i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
-    counter++
-    if (counter > images.length) {
-        counter = 1;
-    }
-    images[counter-1].style.display = "block";
-    setTimeout(displayImages, 3000);
+function displayImages() {
+  const images = document.getElementsByClassName('image');
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+  }
+  counter++;
+  if (counter > images.length) {
+    counter = 1;
+  }
+  images[counter - 1].style.display = 'block';
+  setTimeout(displayImages, 3000);
 }
+
+
